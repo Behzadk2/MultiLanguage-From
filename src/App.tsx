c453066a651ee0a91a1af615cfff53fd7b35ebe3
behzadk2 <bkazemi1994@gmail.com>
2024-08-04 18:06:30 +0200

    style : add css to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
 
+import React from 'react'
 import './App.css'
 import Login from './components/Login'
 
 function App() {
   
-
+  const onChangeLang=(e:React.ChangeEvent<HTMLSelectElement>)=>{
+    const {value}=e.target;
+  }
   return (
     <>
+    <div className='header'>
+      <select onChange={onChangeLang} >
+        <option value="English">English</option>
+      </select>
+    </div>
    <Login></Login>
+   
     </>
   )
 }
